db33993643998186dedad02c8e7b5e0909df0eb8
frankkwabenaaboagye <frank.aboagye@amalitech.com>
2024-12-18 13:17:01 +0000

    inventory management system

---
+++ IMS/src/main/java/frank/ims/model/Order.java
@@ -0,0 +1,20 @@
+package frank.ims.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long productId;
+    private int quantity;
+    private String status; // PENDING, COMPLETED
+}
